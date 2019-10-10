c2cb2d347d1d88195fef8bab26fd2fb3f7f6c081
Łukasz Stalmirski <lucasus1129@gmail.com>
2019-10-10 21:14:57 +0200

    Add Windows configuration

--- Arguments.cpp
+++ Arguments.cpp
@@ -1,6 +1,174 @@
 #include "Arguments.h"
-#include <getopt.h>
 #include <ostream>
+
+#if __has_include(<getopt.h>)
+#include <getopt.h>
+#else
+
+#define no_argument 0
+#define required_argument 1
+#define optional_argument 2
+
+struct option
+{
+    const char* name;
+    int has_arg;
+    int* flag;
+    int val;
+};
+
+namespace
+{
+    static char* optarg = nullptr;
+    static int optind = 1;
+    static int opterr = 0;
+    static int optopt = 0;
+
+    /***********************************************************************************\
+
+    Function:
+        _internal_getopt_init
+
+    Description:
+        Initialize global getopt state.
+
+    \***********************************************************************************/
+    inline void _internal_getopt_init()
+    {
+        optarg = nullptr;
+        optind = 1;
+        opterr = 0;
+        optopt = 0;
+    }
+
+    /***********************************************************************************\
+
+    Function:
+        _internal_getopt_missing_argument
+
+    Description:
+        Report a missing argument for the option and move to the next option.
+
+    \***********************************************************************************/
+    inline int _internal_getopt_missing_argument( const option*, const char* shortopts )
+    {
+        // Missing argument for the option
+        if( shortopts && *shortopts == ':' ) return ':';
+        else return '?';
+    }
+
+    /***********************************************************************************\
+
+    Function:
+        _internal_getopt_unrecognized
+
+    Description:
+        Report not recognized option.
+
+    \***********************************************************************************/
+    inline int _internal_getopt_unrecognized( const char* opt )
+    {
+        // Move to the next option
+        optind++;
+        fprintf( stderr, "getopt: option %s not recognized\n", opt );
+        return '?';
+    }
+
+    /***********************************************************************************\
+
+    Function:
+        getopt_long_only
+
+    Description:
+        Parse next command-line argument.
+
+    \***********************************************************************************/
+    inline int getopt_long_only( int argc, char* const* argv, const char* shortopts,
+        const option* longopts, int* longind )
+    {
+        if( optind == 0 ) _internal_getopt_init();
+
+        int retval = -1;
+
+        // Check if there are any options to parse
+        if( optind >= argc ) return retval;
+
+        const char* opt = argv[optind];
+
+        // Option must start with '-' character
+        if( opt[0] != '-' ) return retval;
+        if( opt[1] == '-' ) opt += 2;
+        else opt++;
+
+        // Single '-' and '--' strings are not options
+        if( opt[0] == 0 ) return retval;
+
+        optarg = nullptr;
+        opterr = 0;
+        optopt = 0;
+
+        retval = 0;
+
+        // Search long options first
+        for( const option* longopt = longopts; longopt && longopt->name; ++longopt )
+        {
+            if( strcmp( longopt->name, opt ) == 0 )
+            {
+                // Valid option found, advance the pointer
+                optind++;
+
+                // If longopt->flag is not NULL, store val at that address instead of returning it
+                if( !longopt->flag ) retval = longopt->val;
+                else *longopt->flag = longopt->val;
+
+                if( longopt->has_arg != no_argument )
+                {
+                    if( optind == argc && longopt->has_arg == required_argument )
+                    {
+                        // Commandline ends unexpectedly
+                        return _internal_getopt_missing_argument( longopt, shortopts );
+                    }
+
+                    if( optind < argc )
+                    {
+                        // Store pointer to the option argument in the optarg and advance the pointer
+                        optarg = argv[optind++];
+                    }
+                }
+                return retval;
+            }
+        }
+
+        // Check if option is unrecognized long option
+        if( opt[1] != 0 ) return _internal_getopt_unrecognized( argv[optind] );
+
+        // Search short options
+        for( const char* shortopt = shortopts; shortopt && *shortopt; ++shortopt )
+        {
+            // Current character in shortopts may be extension to the previous option
+            if( *shortopt == ':' ) continue;
+
+            if( *shortopt == *opt )
+            {
+                // Valid option found, advance the pointer
+                optind++;
+
+                if( shortopt[1] == ':' && optind < argc )
+                {
+                    // Store pointer to the option argument in the optarg and advance the pointer
+                    optarg = argv[optind++];
+                }
+
+                return *opt;
+            }
+        }
+
+        // Option not found
+        return _internal_getopt_unrecognized( argv[optind] );
+    }
+}
+
+#endif
 
 const char RT::CommandLineArguments::s_pShortOptions[] = "i:o:";
 const option RT::CommandLineArguments::s_pLongOptions[] = {
@@ -82,7 +250,7 @@ RT::CommandLineArguments RT::CommandLineArguments::Parse( int argc, char* const*
     optind = 1;
 
     int opt = 0;
-    while( (opt = getopt_long_only( argc, argv, s_pShortOptions, s_pLongOptions, 0 )) != -1 )
+    while( (opt = getopt_long_only( argc, argv, s_pShortOptions, s_pLongOptions, nullptr )) != -1 )
     {
         switch( opt )
         {
